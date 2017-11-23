fd616d84c6be9fbb8d6e8ab62d29191d66bef918
do <395614269@163.com>
2017-11-23 18:49:06 +0800

    tornado 异步web请求

---
+++ rabbitMQ_example/10.receive_to_db.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'do'
+__mtime__ = '2017/11/6'
+__content__ = '失败重写机制，断线重连机制'
+"""
+
+import pika
+credentials = pika.PlainCredentials('zyl','pwd_zyl')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(credentials=credentials,
+    host='112.74.75.38', port=5670, socket_timeout=2, blocked_connection_timeout=2))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+channel.start_consuming()
